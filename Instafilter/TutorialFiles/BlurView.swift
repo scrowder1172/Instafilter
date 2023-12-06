d5710e80e8f26427942ec76a5d9a61e08e55d60e
Scott Crowder <scrowder@gmail.com>
2023-12-06 14:29:07 -0600

    create view to blur message

---
+++ Instafilter/TutorialFiles/BlurView.swift
@@ -0,0 +1,44 @@
+//
+//  BlurView.swift
+//  Instafilter
+//
+//  Created by SCOTT CROWDER on 12/6/23.
+//
+
+import SwiftUI
+
+struct BlurView: View {
+    
+    @State private var blurAmount: Double = 0.0
+    
+    var body: some View {
+        VStack {
+            Text("Secret message")
+                .blur(radius: blurAmount)
+            
+            HStack {
+                Text("Hide Message:")
+                Slider(value: $blurAmount, in: 0...20)
+            }
+            
+            Button("Random Blur") {
+                blurAmount = Double.random(in: 0...20)
+            }
+            .frame(width: 200, height: 50)
+            .background(.yellow)
+            .clipShape(.rect(cornerRadius: 10))
+            
+            Button("Show Message") {
+                blurAmount = 0.0
+            }
+            .frame(width: 200, height: 50)
+            .background(.yellow)
+            .clipShape(.rect(cornerRadius: 10))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    BlurView()
+}
