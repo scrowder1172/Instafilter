0ab7f292a36ff7c620c0a003d280a37ddb88419f
Scott Crowder <scrowder@gmail.com>
2023-12-06 09:15:00 -0600

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by SCOTT CROWDER on 12/6/23.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
