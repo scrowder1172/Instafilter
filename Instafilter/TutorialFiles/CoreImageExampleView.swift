0e498aa73eb1d0416f925ce5a06d5bbd0105ea85
Scott Crowder <scrowder@gmail.com>
2023-12-08 10:16:34 -0600

    add file

---
+++ Instafilter/TutorialFiles/CoreImageExampleView.swift
@@ -0,0 +1,18 @@
+//
+//  CoreImageExampleView.swift
+//  Instafilter
+//
+//  Created by SCOTT CROWDER on 12/8/23.
+//
+
+import SwiftUI
+
+struct CoreImageExampleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoreImageExampleView()
+}
