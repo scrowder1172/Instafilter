84324c3dd2312498f43cd52f187115a69cbfdcb3
Scott Crowder <scrowder@gmail.com>
2023-12-08 18:36:05 -0600

    created view with optional binding float value

--- Instafilter/TutorialFiles/CoreImageExampleView.swift
+++ Instafilter/TutorialFiles/CoreImageExampleView.swift
@@ -16,65 +16,27 @@ struct CoreImageExampleView: View {
     @State private var pixellateOutputImage: Image?
     @State private var crystallizeOutputImage: Image?
     
-    @State private var sepiaToneAdjustment: Float = 1.0
-    @State private var pixellateAdjustment: Float = 20.0
-    @State private var crystallizeAdjustment: Float = 100.0
+    @State private var sepiaToneAdjustment: Float? = 1.0
+    @State private var pixellateAdjustment: Float? = 20.0
+    @State private var crystallizeAdjustment: Float? = 100.0
     
     var body: some View {
         NavigationStack {
             ScrollView {
-                VStack {
-                    Text("SwiftUI Image")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    swiftUIImage?
-                        .resizable()
-                        .scaledToFit()
-                }
-                .padding()
-                .border(.black)
-                .padding(.horizontal)
                 
-                VStack {
-                    Text("Sepia Tone")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    sepiaOutputImage?
-                        .resizable()
-                        .scaledToFit()
-                    Slider(value: $sepiaToneAdjustment, in: 0...1, step: 0.05) {_ in
-                        sepiaToneImage()
-                    }
-                }
-                .padding()
-                .border(.black)
-                .padding(.horizontal)
+                VStackView(text: "SwiftUI Image", image: swiftUIImage) {}
                 
-                VStack {
-                    Text("Pixellation")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    pixellateOutputImage?
-                        .resizable()
-                        .scaledToFit()
-                    Slider(value: $pixellateAdjustment, in: 1...100, step: 5) {_ in
-                        pixellateImage()
-                    }
+                VStackView(text: "Sepia Tone", image: sepiaOutputImage, showSliderView: true, sliderValue: $sepiaToneAdjustment, sliderRange: 0...1, sliderStep: 0.05) {
+                    sepiaToneImage()
                 }
-                .padding()
-                .border(.black)
-                .padding(.horizontal)
                 
-                VStack {
-                    Text("Crystallize")
-                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-                    crystallizeOutputImage?
-                        .resizable()
-                        .scaledToFit()
-                    Slider(value: $crystallizeAdjustment, in: 1...200, step: 10) {_ in
-                        crystallizeImage()
-                    }
+                VStackView(text: "Pixellation", image: pixellateOutputImage, showSliderView: true, sliderValue: $pixellateAdjustment, sliderRange: 1...100, sliderStep: 5) {
+                    pixellateImage()
                 }
-                .padding()
-                .border(.black)
-                .padding(.horizontal)
+                
+                VStackView(text: "Crystallize", image: crystallizeOutputImage, showSliderView: true, sliderValue: $crystallizeAdjustment, sliderRange: 1...200, sliderStep: 10) {
+                    crystallizeImage()
+                }
                 
             }
             .onAppear(perform: {
@@ -101,7 +63,7 @@ struct CoreImageExampleView: View {
         let currentFilter = CIFilter.sepiaTone()
         
         currentFilter.inputImage = beginImage
-        currentFilter.intensity = sepiaToneAdjustment
+        currentFilter.intensity = sepiaToneAdjustment ?? 0
         
         guard let outputImage = currentFilter.outputImage else { return }
         
@@ -120,7 +82,7 @@ struct CoreImageExampleView: View {
         let currentFilter = CIFilter.pixellate()
         
         currentFilter.inputImage = beginImage
-        currentFilter.scale = pixellateAdjustment
+        currentFilter.scale = pixellateAdjustment ?? 1
         
         guard let outputImage = currentFilter.outputImage else { return }
         
@@ -139,7 +101,7 @@ struct CoreImageExampleView: View {
         let currentFilter = CIFilter.crystallize()
         
         currentFilter.inputImage = beginImage
-        currentFilter.radius = crystallizeAdjustment
+        currentFilter.radius = crystallizeAdjustment ?? 1
         
         guard let outputImage = currentFilter.outputImage else { return }
         
@@ -150,6 +112,61 @@ struct CoreImageExampleView: View {
         crystallizeOutputImage = Image(uiImage: uiImage)
     }
 }
+
+struct VStackView: View {
+    let text: String
+    let image: Image?
+    let showSliderView: Bool
+    let sliderRange: ClosedRange<Float>
+    let sliderStep: Float
+    
+    @Binding var sliderValue: Float?  // Binding to an optional Float
+    @State private var internalSliderValue: Float = 0 // Internal state for slider value
+
+    var action: () -> Void
+    
+    // Computed property to get the effective slider value
+    private var effectiveSliderValue: Binding<Float> {
+        Binding<Float>(
+            get: {
+                self.sliderValue ?? self.internalSliderValue
+            },
+            set: {
+                self.sliderValue = $0
+                self.internalSliderValue = $0
+            }
+        )
+    }
+
+    init(text: String, image: Image?, showSliderView: Bool = false, sliderValue: Binding<Float?> = .constant(nil), sliderRange: ClosedRange<Float> = 0...1, sliderStep: Float = 1.0, action: @escaping () -> Void) {
+        self.text = text
+        self.image = image
+        self.showSliderView = showSliderView
+        self._sliderValue = sliderValue
+        self.sliderRange = sliderRange
+        self.sliderStep = sliderStep
+        self.action = action
+    }
+
+    var body: some View {
+        VStack {
+            Text(text)
+                .font(.title)
+            image?
+                .resizable()
+                .scaledToFit()
+            if showSliderView {
+                Slider(value: effectiveSliderValue, in: sliderRange, step: sliderStep) {_ in 
+                    action()
+                }
+            }
+        }
+        .padding()
+        .border(Color.black)
+        .padding(.horizontal)
+    }
+}
+
 
 #Preview {
     CoreImageExampleView()
