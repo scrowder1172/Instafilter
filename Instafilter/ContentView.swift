0f75142055ff15854af98a3a0e58fb5135151995
Scott Crowder <scrowder@gmail.com>
2023-12-13 09:13:43 -0600

    build initial project UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,8 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var processedImage: Image?
+    @State private var filterIntensity: Float = 0.5
+    
     var body: some View {
-        PhotoLibraryExample()
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                if let processedImage {
+                    processedImage
+                        .resizable()
+                        .scaledToFit()
+                } else {
+                    ContentUnavailableView {
+                        Label("No Picture", systemImage: "square.and.pencil.circle.fill")
+                    } description: {
+                        Text("Tap to import a photo")
+                    }
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+                
+                HStack {
+                    Button("Change filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "square.and.arrow.up")
+                }
+            }
+            .navigationTitle("InstaFilter")
+            .padding([.horizontal, .bottom])
+        }
+    }
+    
+    func changeFilter() {
+        
     }
 }
 
